5a498f7f93890f325a5a84f5c0a01d30a0540828
next2014 <83329265@qq.com>
2019-06-14 01:44:31 +0800

    0614

--- vue_demo/src/App.vue
+++ vue_demo/src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+   <div>
+     <img class="logo" src="./assets/logo.png" alt="logo">
+      <!--3.使用组件标签-->
+     <HelloWorld />
+   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld'
+  //1. 引入组件
+  import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+     //2. 映射组件标签
+     components: {
+         HelloWorld
+     }
+
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .logo{
+      width: 200px;
+      height: 200px;
+  }
 </style>
