b9ddea307d014b52074e3ee37882233ab7811b55
liangjialei <liangjialei@liangjialeideMacBook-Pro.local>
2019-06-16 02:03:18 +0800

    616

--- vue_demo/src/main.js
+++ vue_demo/src/main.js
@@ -1,11 +1,10 @@
-/*
-入口JS 创建vue实例
-*/
 import Vue from 'vue'
 import App from './App.vue'
 
 new Vue({
-  el: '#app',
-  components: {App},
-  template: '<App/>'
+    el: '#app',
+    components: {
+        App
+    },
+    template: '<App/>'
 })
