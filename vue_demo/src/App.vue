5967103b0dd591ba002e7a6af7d068b7538168de
liangjialei <liangjialei@liangjialeideMacBook-Pro.local>
2019-06-17 04:21:14 +0800

    0616

--- vue_demo/src/App.vue
+++ vue_demo/src/App.vue
@@ -1,55 +1,44 @@
 <template>
-<<<<<<< HEAD
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div class="todo-container">
+   <div class="todo-wrap">
+     <TodoHeader :addTodo="addTodo"/>
+     <TodoList :todos="todos"/>
+    <TodoFooter/>
+   </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import TodoHeader from './components/TodoHeader.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-=======
-<div class="todo-container">
-    <div class="todo-wrap">
-       <TodoHeader/>
-       <TodoList/>
-       <TodoFooter/>
-    </div>
-  </div>
-</template>
-
-<script>
-import TodoHeader from './components/TodoHeader'
-import TodoList from './components/TodoList'
-import TodoFooter from './components/TodoFooter'
-export default {
-    components: {
-         TodoHeader,
-         TodoList,
-         TodoFooter
+  
+  data () {
+    return {
+      todos: [
+        {title: '吃饭', complete: false},
+        {title: '睡觉', complete: true},
+        {title: 'coding', complete: false}
+      ]
     }
->>>>>>> 39d63b89c68a085a4d7b3fb67d440536ac9c0471
+  },
+  
+  methods: {
+    addTodo (todo) {
+      this.todos.unshift(todo)
+    }
+  },
+
+  components: {
+    TodoHeader,
+    TodoList,
+    TodoFooter
+  }
 }
 </script>
 
 <style>
-<<<<<<< HEAD
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-=======
-
   /*app*/
   .todo-container {
     width: 600px;
@@ -60,7 +49,4 @@ export default {
     border: 1px solid #ddd;
     border-radius: 5px;
   }
-
-  
->>>>>>> 39d63b89c68a085a4d7b3fb67d440536ac9c0471
 </style>
--- vue_demo/src/main.js
+++ vue_demo/src/main.js
@@ -1,28 +1,9 @@
-<<<<<<< HEAD
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-=======
->>>>>>> 39d63b89c68a085a4d7b3fb67d440536ac9c0471
 import Vue from 'vue'
-import App from './App'
-
-<<<<<<< HEAD
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
-=======
+import App from './App.vue'
 import './base.css'
 
 new Vue({
-    el: '#app',
-    components: {
-        App
-    },
-    template: '<App/>'
+  el: '#app',
+  components: {App},
+  template: '<App/>'
 })
->>>>>>> 39d63b89c68a085a4d7b3fb67d440536ac9c0471
