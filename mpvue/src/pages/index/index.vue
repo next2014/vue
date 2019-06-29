cf14e8141db34d5facf3f0fa5455adf3025edd10
next2014 <83329265@qq.com>
2019-06-30 00:38:32 +0800

    0701

---
+++ mpvue/src/pages/index/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img src="" alt="">
+    <p>hello mpvue</p>
+    <div>
+      <p>开启小程序之旅</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
