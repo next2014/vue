3283a99d38deabd18ce7def1b90e3aeb41c9c90f
next2014 <83329265@qq.com>
2019-07-17 17:54:34 +0800

    0717

--- wx_miniProject/mpvue_omc/src/pages/home/home.vue
+++ wx_miniProject/mpvue_omc/src/pages/home/home.vue
@@ -2,22 +2,35 @@
  <div>
    <div class="topNav">
      <ul>
-       <li>推荐</li>
-       <li>好吃的</li>
-       <li>好玩的</li>
-       <li>活动</li>
-       <li>更多</li>
-       <li>全部</li>
+       <li class="active"><a href="">推荐</a></li>
+       <li><a href="">好吃的</a></li>
+       <li><a href="">好玩的</a></li>
+       <li><a href="">活动</a></li>
+       <li><a href="">更多</a></li>
+       <li><a href="">全部</a></li>
       </ul>
       <div class="ad">
         <img src="/static/images/index/home/1_03.jpg" alt="">
       </div>
       <div class="noice">
-        <p><img src="/static/images/icon/share.png" alt=""> <span>公告</span> 49.9元超值价抢购欧美城海洋世界..</p>
+        <p><img src="/static/images/index/icon/noice.png" alt=""> <span>公告</span> 49.9元超值价抢购欧美城海洋世界..</p>
       </div>
    </div>
-   <div class="user">用户</div>
-    <div class="project">
+   <div class="user">
+      <div class="user_left">
+         <img src="/static/images/index/1_05_05.jpg" alt="">
+         <p>用户名</p>
+         <button>未加入会员卡</button>
+      </div>
+      <div class="user_right">
+        <ul>
+          <li><p>1</p><span>订单</span></li>
+          <li><p>0</p><span>红包</span></li>
+          <li><p>0</p><span>积分</span></li>
+        </ul>
+      </div>
+   </div>
+   <div class="project">
       <ul>
          <li>
            <img src="/static/images/index/icon/1_06_03.jpg" alt="">
@@ -53,26 +66,129 @@
          </li>
       </ul>
    </div>
-   <div class="module01">
-      <div class="title">最新活动</div>
+   <div class="module newHd">
+      <div class="newHd_tit">
+         <div class="newHd_tit_l">
+           <img src="/static/images/index/icon/noice.png" alt="">
+           <h2>最新活动</h2>
+           <div class="newHd_tit_tab"><span>亲子游</span> <span>夏令营</span></div>
+           <div class="line"></div>
+         </div>
+         <div class="newHd_page">
+           <span class="start">5</span> / <span>1</span>
+         </div>
+      </div>
+      <div class="newHd_con">
+         <img src="/static/images/index/home/1_07_03.jpg" alt="">
+         <div class="newHd_con_tit">【六一节】亲子趣味嘉年华&童趣缤纷乐园！最好玩的儿童节安排好了！</div>
+         <div class="newHd_price">
+             <span>￥30.00</span>
+             <button>立即购买</button>
+         </div>
+      </div>
    </div>
-   <div class="module02">
-      <div class="title">在线购票</div>
+   <div class="module">
+      <div class="newHd_tit">
+         <div class="newHd_tit_l">
+           <img src="/static/images/index/icon/noice.png" alt="">
+           <h2>在线购票</h2>
+           <div class="line"></div>
+         </div>
+         <div class="newHd_more">
+           <a href="">查看更多>></a>
+         </div>
+      </div>
+      <div class="newHd_con">
+         <!-- <p>在线购票内容</p> -->
+         <div class="newHd_piao">
+            <ul>
+               <li><img src="/static/images/index/home/1_08_07.jpg" alt="">
+                  <p>【六一节】亲子趣味嘉年华&童趣缤纷乐园</p>
+                  <p><span>￥30起</span><button>立刻购买</button></p>
+               </li>
+               <li><img src="/static/images/index/home/1_08_09.jpg" alt="">
+                  <p>【六一节】亲子趣味嘉年华&童趣缤纷乐园</p>
+                  <p><span>￥30起</span><button>立刻购买</button></p>
+               </li>
+            </ul>
+         </div>
+         <div class="newHd_piao"></div>
+      </div>
    </div>
-   <div class="module03">
-      <div class="title">游玩推荐</div>
+   <div class="module">
+      <div class="newHd_tit">
+         <div class="newHd_tit_l">
+           <img src="/static/images/index/icon/noice.png" alt="">
+           <h2>游玩推荐</h2>
+           <div class="line"></div>
+         </div>
+         <div class="newHd_more">
+           <a href="">查看更多>></a>
+         </div>
+      </div>
+      <div class="newHd_con mouse">
+         <p>在线购票内容</p>
+      </div>
    </div>
-   <div class="module04">
-      <div class="title">最新资讯</div>
+   <div class="module">
+      <div class="newHd_tit">
+         <div class="newHd_tit_l">
+           <img src="/static/images/index/icon/noice.png" alt="">
+           <h2>最新资讯</h2>
+           <div class="line"></div>
+         </div>
+         <div class="newHd_more">
+           <a href="">查看更多>></a>
+         </div>
+      </div>
+      <div class="newHd_con mouse">
+         <p>在线购票内容</p>
+      </div>
    </div>
-   <div class="module05">
-      <div class="title">美食天地</div>
+   <div class="module">
+      <div class="newHd_tit">
+         <div class="newHd_tit_l">
+           <img src="/static/images/index/icon/noice.png" alt="">
+           <h2>美食天地</h2>
+           <div class="line"></div>
+         </div>
+         <div class="newHd_more">
+           <a href="">查看更多>></a>
+         </div>
+      </div>
+      <div class="newHd_con mouse">
+         <p>在线购票内容</p>
+      </div>
    </div>
-   <div class="module06">
-      <div class="title">休闲娱乐</div>
+   <div class="module">
+      <div class="newHd_tit">
+         <div class="newHd_tit_l">
+           <img src="/static/images/index/icon/noice.png" alt="">
+           <h2>休闲娱乐</h2>
+           <div class="line"></div>
+         </div>
+         <div class="newHd_more">
+           <a href="">查看更多>></a>
+         </div>
+      </div>
+      <div class="newHd_con mouse">
+         <p>在线购票内容</p>
+      </div>
    </div>
-   <div class="module07">
-      <div class="title">欧欧主题酒店</div>
+   <div class="module">
+      <div class="newHd_tit">
+         <div class="newHd_tit_l">
+           <img src="/static/images/index/icon/noice.png" alt="">
+           <h2>欧欧主题酒店</h2>
+           <div class="line"></div>
+         </div>
+         <div class="newHd_more">
+           <a href="">查看更多>></a>
+         </div>
+      </div>
+      <div class="newHd_con mouse">
+         <p>在线购票内容</p>
+      </div>
    </div>
  </div>
 </template>
@@ -81,42 +197,112 @@ export default {}
 </script>
 
 <style>
+  .mouse p{
+     min-height: 500rpx;
+     background-color: #dddddd;
+  }
   .topNav{
       width: 100%;
+      background-color: #FDE024;
+  }
+  .topNav ul{
+     display: flex;
+     justify-content: center;
+     align-items: center;
   }
   .topNav li{
+     width:16.3%;
      float: left;
+     text-align: center;
+     font-size: 28.96rpx;
+  }
+  .topNav li.active a{
+     font-size: 36.2rpx;
+  }
+  .user{
+     /* width: 100%; */
+     height: 140rpx;
+     display: flex;
+     justify-content:space-between;
+     align-items: center;
+     margin:0 40rpx 20rpx 40rpx;
+     padding-top: 40rpx;
+     padding-bottom: 40rpx;
+     border-bottom: 1px solid #E6E6E6;
+  }
+  .user .user_left{
+     width:60%;
+  }
+  .user .user_right{
+     width:40%;
+     display: flex;
+     justify-content: space-around;
+  }
+  .user .user_right li{
+     float: left;
+     padding: 15rpx;
+     text-align: center;
+  }
+  .user .user_right p{
+     font-size: 35rpx;
+  }
+  .user .user_right span{
+     font-size: 30rpx;
+  }
+  .user .user_left img{
+     width: 115rpx;
+     height: 115rpx;
+     float: left;
+     margin-right:20rpx;
+  }
+  .user .user_left p{
+     height: 60rpx;
+     line-height: 60rpx;
+     /* margin:0 0 0 10rpx; */
+  }
+  .user .user_left button{
+     width: 140rpx;
+     height: 50rpx;
+     color: #666666;
+     font-size: 20rpx;
+     padding: 0;
+     margin:0;
   }
   .ad{
-     height: 160rpx;
+     height: 130rpx;
   }
   .ad img{
      width: 100%;
-     height: 160rpx;
+     height: 123rpx;
   }
   .noice {
      background-color: #FDE024;
   }
   .noice p{
-     width: 90%;
+     width: 100%;
      height: 60rpx;
+     line-height: 60rpx;
      overflow: hidden;
      padding:0 20rpx;
+     font-size: 25.34rpx;
   }
   .noice img{
-     width:45rpx;
-     height:45rpx;
+     width: 35rpx;
+     height: 35rpx;
+     margin-right:10rpx;
      vertical-align: middle;
   }
   .project{
      width: 100%;
-     height: 500rpx;
-     padding:0 20rpx;
+     height: 352.95rpx;
+     display: flex;
+     justify-content: center;
+     align-items: center;
   }
   .project li{
      width:150rpx;
      float: left;
-     padding:0 15rpx 40rpx 15rpx;
+     padding:0 18rpx 30rpx 18rpx;
      text-align: center;
   }
   .project li p{
@@ -124,8 +310,92 @@ export default {}
      color: #333333;
   }
   .project img{
-     width: 100rpx;
-     height: 100rpx;
+     width: 81.45rpx;
+     height: 81.45rpx;
      margin:0 auto;
+  }
+  .newHd_tit{
+     display: flex;
+     justify-content: space-between;
+     align-items: center;
+     padding:10rpx 20rpx;
+  }
+  .newHd_tit_l{
+     display: flex;
+     align-items: center;
+     position: relative;
+  }
+  .newHd_tit_l .line{
+     width: 225rpx;
+     height: 15rpx;
+     border-radius: 20rpx;
+     background-color: #FDE024;
+     position: absolute;
+     bottom: 0;
+     left: 0;
+     z-index: -1;
+  }
+  .newHd_tit_l h2{
+     font-size: 40rpx;
+     font-weight: bold;
+  }
+  .newHd_tit_l span{
+     margin-right: 20rpx;
+  }
+  .newHd_tit_tab{
+     padding-left:50rpx;
+  }
+  .newHd_tit_tab span{
+     font-size: 25rpx;
+     margin-right: 40rpx;
+  }
+  .newHd_page{
+      color: #D7D7D7;
+  }
+  .newHd_page .start{
+     font-size: 40rpx;
+     color: #333333;
+  }
+  .newHd_more{
+     font-size: 25rpx;
+     color: #666666;
+  }
+  .newHd_tit img{
+     width: 45rpx;
+     height: 45rpx;
+     margin-right: 5rpx;
+  }
+  .newHd_con {
+     padding: 20rpx;
+  }
+  .newHd_con img{
+     width: 100%;
+     height: 360rpx;
+  }
+  .newHd_con .newHd_con_tit{
+     padding: 20rpx 20rpx 0 20rpx;
+  }
+  .newHd_price{
+     display: flex;
+     justify-content: space-between;
+     align-items: center;
+     padding: 20rpx;
+  }
+  .newHd_price button{
+     margin: 0;
+     font-size: 25rpx;
+     background-color: rgb(246, 172, 0);
+     color: #ffffff;
+     border: 0;
+     border-radius: 50rpx;
+  }
+  .newHd_piao ul{
+     display: flex;
+     align-items: center;
+     justify-content: space-between;
+  }
+  .newHd_piao li{
+     width: 48%;
+     height: 217rpx;
   }
 </style>
